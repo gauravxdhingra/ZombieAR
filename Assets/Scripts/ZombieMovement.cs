0b14863a36856ac6fd3aef65311a4c144e84f62f
gauravxdhingra <grvdhingra1999@gmail.com>
2020-06-13 06:02:54 +0530

    Zombie Behaviour

---
+++ Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour
+{
+    public GameObject Camera;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Translate(Vector3.forward * Time.deltaTime *0.3f);
+        transform.LookAt(Camera.transform.position);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+    }
+}
